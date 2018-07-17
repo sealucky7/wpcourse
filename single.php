774765d5bbe4844822929ca7e291357443677d9d
sea-lucky <sea.yastreb@gmail.com>
2018-07-17 22:45:51 +0300

    add question and fix some bugs

--- single.php
+++ single.php
@@ -23,66 +23,65 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-8 posts-list">
-        <div class="single-post row">
-          <div class="col-lg-12">
-            <div class="feature-img">
-              <img class="img-fluid" src="<?php echo P_IMG_DIR ?>blog/feature-img1.jpg" alt="">
-            </div>
+
+        <?php if ( have_posts()): ?>
+          <?php while ( have_posts()): the_post();?>
+
+          <div class="single-post row">
+        <div class="col-lg-12">
+          <div class="feature-img">
+              <?php the_post_thumbnail(); ?>
+<!--              <img class="img-fluid" src="--><?php //echo P_IMG_DIR ?><!--blog/feature-img1.jpg" alt="">-->
           </div>
-          <div class="col-lg-3  col-md-3 meta-details">
-            <ul class="tags">
-              <li><a href="#">Food,</a></li>
-              <li><a href="#">Technology,</a></li>
-              <li><a href="#">Politics,</a></li>
-              <li><a href="#">Lifestyle</a></li>
+        </div>
+        <div class="col-lg-3  col-md-3 meta-details">
+          <ul class="tags">
+              <?php the_category(', '); ?>
+          </ul>
+          <div class="user-details row">
+            <p class="user-name col-lg-12 col-md-12 col-6"><a href="#"><?php echo get_the_author(); ?></a> <span class="lnr lnr-user"></span></p>
+            <p class="date col-lg-12 col-md-12 col-6"><a href="#"><?php echo get_the_date(); ?></a> <span class="lnr lnr-calendar-full"></span></p>
+            <p class="view col-lg-12 col-md-12 col-6"><a href="#">1.2M Views</a> <span class="lnr lnr-eye"></span></p>
+            <p class="comments col-lg-12 col-md-12 col-6"><a href="#">06 Comments</a> <span class="lnr lnr-bubble"></span></p>
+            <ul class="social-links col-lg-12 col-md-12 col-6">
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-github"></i></a></li>
+              <li><a href="#"><i class="fa fa-behance"></i></a></li>
             </ul>
-            <div class="user-details row">
-              <p class="user-name col-lg-12 col-md-12 col-6"><a href="#">Mark wiens</a> <span class="lnr lnr-user"></span></p>
-              <p class="date col-lg-12 col-md-12 col-6"><a href="#">12 Dec, 2017</a> <span class="lnr lnr-calendar-full"></span></p>
-              <p class="view col-lg-12 col-md-12 col-6"><a href="#">1.2M Views</a> <span class="lnr lnr-eye"></span></p>
-              <p class="comments col-lg-12 col-md-12 col-6"><a href="#">06 Comments</a> <span class="lnr lnr-bubble"></span></p>
-              <ul class="social-links col-lg-12 col-md-12 col-6">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-github"></i></a></li>
-                <li><a href="#"><i class="fa fa-behance"></i></a></li>
-              </ul>
-            </div>
           </div>
-          <div class="col-lg-9 col-md-9">
-            <h3 class="mt-20 mb-20">Astronomy Binoculars A Great Alternative</h3>
-            <p class="excert">
-              MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction.
-            </p>
-            <p>
-              Boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed
-            </p>
-            <p>
-              Boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed
-            </p>
+        </div>
+        <div class="col-lg-9 col-md-9">
+          <h3 class="mt-20 mb-20"><?php the_title(); ?></h3>
+          <?php the_content(); ?>
+        </div>
+        <div class="col-lg-12">
+          <div class="quotes">
+            MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training.
           </div>
-          <div class="col-lg-12">
-            <div class="quotes">
-              MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training.
+          <div class="row mt-30 mb-30">
+            <div class="col-6">
+              <img class="img-fluid" src="<?php echo P_IMG_DIR ?>blog/post-img1.jpg" alt="">
             </div>
-            <div class="row mt-30 mb-30">
-              <div class="col-6">
-                <img class="img-fluid" src="<?php echo P_IMG_DIR ?>blog/post-img1.jpg" alt="">
-              </div>
-              <div class="col-6">
-                <img class="img-fluid" src="<?php echo P_IMG_DIR ?>blog/post-img2.jpg" alt="">
-              </div>
-              <div class="col-lg-12 mt-30">
-                <p>
-                  MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower.
-                </p>
-                <p>
-                  MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower.
-                </p>
-              </div>
+            <div class="col-6">
+              <img class="img-fluid" src="<?php echo P_IMG_DIR ?>blog/post-img2.jpg" alt="">
+            </div>
+            <div class="col-lg-12 mt-30">
+              <p>
+                MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower.
+              </p>
+              <p>
+                MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower.
+              </p>
             </div>
           </div>
         </div>
+      </div>
+
+          <?php endwhile; ?>
+        <?php endif; ?>
+
+
         <div class="navigation-area">
           <div class="row">
             <div class="col-lg-6 col-md-6 col-12 nav-left flex-row d-flex justify-content-start align-items-center">
